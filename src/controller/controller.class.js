34d9a45870ae57459293c2f427732c9ec7c0a2ed
Nuria <nurborgen@alu.edu.gva.es>
2024-05-23 14:28:15 +0200

    controller preparaes

--- src/controller/controller.class.js
+++ src/controller/controller.class.js
@@ -1,4 +1,14 @@
+import ItemsRepository from '../repositories/items.repository'
+import View from '../view/view.class'
 
 export default class Controller {
-    
+    constructor() {
+        this.itemsRepository = new ItemsRepository()
+        this.view = new View()
+        this.item = []
+    }
+
+    async init() {
+
+    }
 }
